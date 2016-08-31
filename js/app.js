2588d765dcb2d54acef74a224be0bff632a6c36b
Fredy Esse <eleegia@gmail.com>
2016-08-31 11:29:29 -0700

    Added arrows, svg

--- js/app.js
+++ js/app.js
@@ -4,26 +4,68 @@ var $image = $("<img>");
 var $caption = $("<p></p>");
 var $title = $("<h1></h1>");
 
+var $nextArrow = $('<div id="nextArrow"></div>');
+var $prevArrow = $('<div id="prevArrow"></div>');
+
 $overlay.append($image);		/* Adds image to overlay */
 
 $overlay.append($title);
 
 $overlay.append($caption);
 
+$image.before($prevArrow);
+$image.after($nextArrow);
+
 $("body").append($overlay);   /* Adds overlay to body */
 
 
-$("#imageGallery a").click( function(event) {		/* Captures the click event on a link */
+$("#imageGallery a").click(function(event) {		/* Captures the click event on a link */
 	event.preventDefault();
-	var imageLocation = $(this).attr("href");
-	$image.attr("src", imageLocation);
-	var captionTitle = $(this).children("img").attr("title");
-	$title.text(captionTitle);
-	var captionText = $(this).children("img").attr("alt");
-	$caption.text(captionText);
+
+	getCurrentImage(this);
+
 	$overlay.show();
 });
 
-$overlay.click( function(){
+$prevArrow.click(function(event) {
+	getPrevImage(this);
+});
+
+$overlay.click(function() {
+	getNextImage();
+});
+
+
+function getCurrentImage (currentImage) {				/* Gets current image */
+	thisImage = currentImage;
+
+	var imageLocation = $(currentImage).attr("href");		
+	$image.attr("src", imageLocation);
+	
+	var captionTitle = $(currentImage).children("img").attr("title");		/* Sets the title */
+	$title.text(captionTitle);
+	
+	var captionText = $(currentImage).children("img").attr("alt");			/* Sets the captions */
+	$caption.text(captionText);
+};
+
+function getPrevImage() {									// Gets the previous image
+	imageParent = $(thisImage).parent().prev();
+	if(imageParent.length!=0) {
+		thisImage = $(imageParent.children("a"));
+	}
+	getCurrentImage(thisImage);
+};
+
+function getNextImage() {
+    imageParent = $(thisImage).parent().next();
+    if(imageParent.length!=0){
+    	thisImage = $(imageParent).children("a");
+    }
+    getCurrentImage(thisImage);
+}
+
+
+$overlay.click(function() {
 	$overlay.hide();
 });
