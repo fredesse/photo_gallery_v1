3c19cfc4d07abfa537162e7523499706ed595564
Fredy Esse <eleegia@gmail.com>
2016-08-31 13:57:26 -0700

    Switched between pics, styled buttons

--- js/app.js
+++ js/app.js
@@ -4,17 +4,17 @@ var $image = $("<img>");
 var $caption = $("<p></p>");
 var $title = $("<h1></h1>");
 
-var $nextArrow = $('<div id="nextArrow"></div>');
-var $prevArrow = $('<div id="prevArrow"></div>');
+var $nextArrow = $('<button id="nextArrow"></button>');
+var $prevArrow = $('<button id="prevArrow"></button>');
 
 $overlay.append($image);		/* Adds image to overlay */
 
-$overlay.append($title);
-
-$overlay.append($caption);
-
 $image.before($prevArrow);
 $image.after($nextArrow);
+
+$overlay.append($title);
+$overlay.append($caption);
+
 
 $("body").append($overlay);   /* Adds overlay to body */
 
@@ -28,13 +28,14 @@ $("#imageGallery a").click(function(event) {		/* Captures the click event on a l
 });
 
 $prevArrow.click(function(event) {
-	getPrevImage(this);
+	event.stopPropagation();
+	getPrevImage();
 });
 
-$overlay.click(function() {
+$nextArrow.click(function(event) {
+	event.stopPropagation();
 	getNextImage();
 });
-
 
 function getCurrentImage (currentImage) {				/* Gets current image */
 	thisImage = currentImage;
@@ -57,7 +58,7 @@ function getPrevImage() {									// Gets the previous image
 	getCurrentImage(thisImage);
 };
 
-function getNextImage() {
+function getNextImage() {								// Gets the next image
     imageParent = $(thisImage).parent().next();
     if(imageParent.length!=0){
     	thisImage = $(imageParent).children("a");
@@ -66,6 +67,6 @@ function getNextImage() {
 }
 
 
-$overlay.click(function() {
+$overlay.click(function() {			// Hides overlay
 	$overlay.hide();
 });
