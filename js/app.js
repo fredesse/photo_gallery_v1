db97d1c40d00d07f85fa3b456b553440b6fa6106
Fredy Esse <eleegia@gmail.com>
2016-09-01 20:56:44 -0700

    Lightbox done, added search

--- js/app.js
+++ js/app.js
@@ -7,46 +7,46 @@ var $title = $("<h1></h1>");
 var $nextArrow = $('<div id="nextArrow"></div>');
 var $prevArrow = $('<div id="prevArrow"></div>');
 
-$overlay.append($image);		/* Adds image to overlay */
+$overlay.append($image);		// Adds image to overlay
 
-$image.before($prevArrow);
-$image.after($nextArrow);
+$image.before($prevArrow);		// Adds left arrow
+$image.after($nextArrow);		// Adds right arrow
 
 $overlay.append($title);
 $overlay.append($caption);
 
 
-$("body").append($overlay);   /* Adds overlay to body */
+$("body").append($overlay);   // Adds overlay to body
 
 
-$("#imageGallery a").click(function(event) {		/* Captures the click event on a link */
+$("#imageGallery a").click(function(event) {		// Captures the click event on a link
 	event.preventDefault();
 
 	getCurrentImage(this);
 
-	$overlay.show();
+	$overlay.fadeTo('slow', 1).show();
 });
 
-$prevArrow.click(function(event) {
+$prevArrow.click(function(event) {				// Shows previous image when clicked
 	event.stopPropagation();
 	getPrevImage();
 });
 
-$nextArrow.click(function(event) {
+$nextArrow.click(function(event) {				// Shows next image when clicked
 	event.stopPropagation();
 	getNextImage();
 });
 
-function getCurrentImage (currentImage) {				/* Gets current image */
+function getCurrentImage (currentImage) {				// Gets current image
 	thisImage = currentImage;
 
 	var imageLocation = $(currentImage).attr("href");		
 	$image.attr("src", imageLocation);
 	
-	var captionTitle = $(currentImage).children("img").attr("title");		/* Sets the title */
+	var captionTitle = $(currentImage).children("img").attr("title");		// Sets the title
 	$title.text(captionTitle);
 	
-	var captionText = $(currentImage).children("img").attr("alt");			/* Sets the captions */
+	var captionText = $(currentImage).children("img").attr("alt");			// Sets the captions 
 	$caption.text(captionText);
 };
 
@@ -60,7 +60,7 @@ function getPrevImage() {									// Gets the previous image
 
 function getNextImage() {								// Gets the next image
     imageParent = $(thisImage).parent().next();
-    if(imageParent.length!=0){
+    if(imageParent.length!=0) {
     	thisImage = $(imageParent).children("a");
     }
     getCurrentImage(thisImage);
@@ -68,5 +68,5 @@ function getNextImage() {								// Gets the next image
 
 
 $overlay.click(function() {			// Hides overlay
-	$overlay.hide();
+	$overlay.hide('slow');
 });
